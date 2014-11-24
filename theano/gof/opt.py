67d0157315e421998b70f7858d47d3cc9bc93d97
f0k <github@jan-schlueter.de>
2014-11-24 18:03:01 +0100

    Nicer metaopt verbose output and metaopt.verbose config flag to enable it

--- theano/gof/opt.py
+++ theano/gof/opt.py
@@ -823,6 +823,10 @@ class LocalOptimizer(object):
                 (' ' * level), self.__class__.__name__, id(self))
 
 
+theano.configparser.AddConfigVar('metaopt.verbose',
+        "Enable verbose output for meta optimizers",
+        theano.configparser.BoolParam(False), in_c_key=False)
+
 class LocalMetaOptimizer(LocalOptimizer):
     """Base class for meta-optimizers that try a set of LocalOptimizers
     to replace a node and choose the one that executes the fastest"""
@@ -830,7 +834,7 @@ class LocalMetaOptimizer(LocalOptimizer):
     def __init__(self, tracks=None, optimizers=()):
         self._tracks = tracks
         self.optimizers = list(optimizers)
-        self.verbose = False
+        self.verbose = config.metaopt.verbose
 
     def register(self, optimizer):
         self.optimizers.append(optimizer)
@@ -858,14 +862,19 @@ class LocalMetaOptimizer(LocalOptimizer):
                 missing.add(input)
         if missing:
             givens.update(self.provide_inputs(node, missing))
+            missing.difference_update(givens.keys())
         # ensure we have data for all input variables that need it
-        if any(var not in givens for var in missing):
+        if missing:
             if self.verbose:
-                print ("%s skipping %s (cannot create test inputs)" %
-                       (self.__class__.__name__, node))
+                print ("%s cannot meta-optimize %s, "
+                       "%d of %d input shapes unknown" %
+                       (self.__class__.__name__, node, len(missing), node.nin))
             return
         # now we can apply the different optimizations in turn,
         # compile the resulting subgraphs and time their execution
+        if self.verbose:
+            print ("%s meta-optimizing %s (%d choices):" %
+                   (self.__class__.__name__, node, len(self.optimizers)))
         timings = []
         for opt in self.optimizers:
             outputs = opt.transform(node)
@@ -878,15 +887,20 @@ class LocalMetaOptimizer(LocalOptimizer):
                     timing = min(self.time_call(fn) for _ in range(3))
                 except Exception as e:
                     if self.verbose:
-                        print e
+                        print "* %s: exception" % opt, e
                     continue
                 else:
                     if self.verbose:
-                        print opt, timing
-                    timings.append((timing, outputs))
+                        print "* %s: %.5g sec" % (opt, timing)
+                    timings.append((timing, outputs, opt))
+            else:
+                if self.verbose:
+                    print "* %s: not applicable" % opt
         # finally, we choose the fastest one
         if timings:
             timings.sort()
+            if self.verbose:
+                print "= %s" % timings[0][2]
             return timings[0][1]
         return
 
